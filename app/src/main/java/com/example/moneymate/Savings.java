0accc3b79ceb90803549f5ff6a8ad06aea551ce6
Senal Ransara <senal.ransara38@gmail.com>
2021-05-02 21:59:56 +0530

    update navigations between pages

--- app/src/main/java/com/example/moneymate/Savings.java
+++ app/src/main/java/com/example/moneymate/Savings.java
@@ -2,13 +2,31 @@ package com.example.moneymate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Savings extends AppCompatActivity {
 
+    Button addButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_savings);
+
+        addButton = findViewById(R.id.btn_5);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_05 = new Intent(getApplicationContext(),AddSavings.class);
+                startActivity(intent_05);
+
+            }
+        });
+
+
+
     }
 }
