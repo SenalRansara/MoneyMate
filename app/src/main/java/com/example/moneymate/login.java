2bdc1082d1eddd6fe80afd091a80a8ff4e4ced55
Hiruni Arunoda <arunoda749@gmail.com>
2021-05-04 11:14:47 +0530

    created signup and currency converter pages

--- app/src/main/java/com/example/moneymate/login.java
+++ app/src/main/java/com/example/moneymate/login.java
@@ -2,13 +2,46 @@ package com.example.moneymate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
+
+    private EditText loginUsername , loginPassword;
+    private Button loginButton;
+    private DataBaseHandler myDb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginUsername = findViewById(R.id.loginusername);
+        loginPassword = findViewById(R.id.loginpassword);
+        loginButton = findViewById(R.id.loginbutton);
+
+        myDb = new DataBaseHandler(this);
+
+        loginUser();
+    }
+    private void loginUser(){
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean var = myDb.checkUser(loginUsername.getText().toString(), loginPassword.getText().toString());
+                if (var){
+                    Toast.makeText(login.this,"Login Successfully !!" , Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(login.this , Home.class));
+                    finish();
+                }else{
+                    Toast.makeText(login.this,"Login failed !!" , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/moneymate/signup.java
@@ -0,0 +1,68 @@
+package com.example.moneymate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class signup extends AppCompatActivity {
+
+    private EditText emailSignUp , usernameSignUp , passwordSignUp;
+    private Button signUpButton;
+    private DataBaseHandler myDB;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        Button btn1 = (Button)findViewById(R.id.signupbutton);
+        Button btn2 = (Button)findViewById(R.id.BackToSignin);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(signup.this, login.class));
+            }
+
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(signup.this, login.class));
+            }
+        });
+
+
+        //DB handlling for signup
+        emailSignUp = findViewById(R.id.signupemail);
+        usernameSignUp = findViewById(R.id.signupusername);
+        passwordSignUp = findViewById(R.id.signuppassword);
+
+        signUpButton = findViewById(R.id.signupbutton);
+        myDB = new DataBaseHandler(this);
+        insertUser();
+
+    }
+
+    private  void  insertUser(){
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean var = myDB.registerUser(usernameSignUp.getText().toString(), emailSignUp.getText().toString(),passwordSignUp.getText().toString());
+                if (var){
+                    Toast.makeText(signup.this,"User Registered Successfully !!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(signup.this,"Registration Error !!" , Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
